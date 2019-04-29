a8d7d64d4375b9a656f5d70e3d96651350e8a9de
JacksonGresham <49295615+JacksonGresham@users.noreply.github.com>
2019-04-29 14:15:09 +1000

    Update CipherProject.c

--- CipherProject.c
+++ CipherProject.c
@@ -1,16 +1,31 @@
-#include<stdio.h>
-#include<string.h>
+#include<stdio.h> //Library used for 'fget()' and 'stdin'
+#include<string.h> //Library used for 'stringlen()'
 #include<stdlib.h>
 
-main() {
-    char message[] = "Bam";
-    int key;
-    int stringLength(char *message);
-    
-    
+void cipher(char* text, int cipherkey); //Function definition
 
-printf("%s\n", str);
+int main(void){
+	
+	char text[101]; //The variable used for manipulation of the ASCII characters
+    int cipherkey = 1; //By how many letters the text will be switched
+	printf("Enter text"); //Text to be encrypted
+	fgets(text, sizeof(text), stdin); //Uses 'fget' to store text into text variable
+	printf("The ciphered text is : ");
+	caesarcipher(text, cipherkey); //Prints encrypted text
+	system("pause");
+}
 
-
-return 0;
+void caesarcipher(char* text, int cipherkey){ //Function prototype
+	
+	int i=0;
+	int ciphervalue;
+	char cipher;
+	
+	while( text[i] != '\0' && strlen(text)-1 > i){ //Takes one letter from the text to encrypt and limits the number of times this encryption occurs by using the string length
+		ciphervalue = ((int)text[i] -97 + cipherkey) % 26 + 97; //Makes it so that the ASCII characters follow a=1, b=2 etc. and then reconverts it back to letters once the encryption is done
+		cipher = (char)(ciphervalue);
+		printf("%c", cipher);
+		i++;
+	}
+	printf("\n");
 }
